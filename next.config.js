5c34eca8332b5b4e2f7b9d6f039374b9d759bef3
shubhamfartade96 <shubhamfartade96@gmail.com>
2025-09-19 14:19:59 +0530

    Configure SSR deploy with webframeworks experiment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Remove or comment out output: 'export'
+  // output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
-  eslint: { ignoreDuringBuilds: true },      // ignore ESLint errors during CI build
-  // remove any experimental.serverActions entry entirely
-};
+  eslint: { ignoreDuringBuilds: true }
+  // Do not disable serverActions here
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
